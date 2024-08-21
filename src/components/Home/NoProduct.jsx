d4efa9817e04cb6333df0e5a42f267c556ce18ab
redwanrahat22 <redwan.rahat69@gmail.com>
2024-08-21 20:30:24 +0600

    edited the error page

--- src/components/Home/NoProduct.jsx
+++ src/components/Home/NoProduct.jsx
@@ -6,10 +6,10 @@ const NoProduct = () => {
     const {screenmode} = useContext(AuthContex)
 
     return (
-        <div className="mt-20 h-screen">
+        <div className="md:mt-20 h-screen">
             <div className="text-center text-xl ">
-            <img className={`w-48 m-auto ${screenmode ? 'bg-black brightness-150 saturate-150' : 'bg-transparent'}`} src="https://i.postimg.cc/02GSWhBw/oops-logo-final.webp" alt="" />
-           <div className="mt-8 font-menu">
+            <img className={`md:w-48 w-24 m-auto ${screenmode ? 'bg-black brightness-150 saturate-150' : 'bg-transparent'}`} src="https://i.postimg.cc/02GSWhBw/oops-logo-final.webp" alt="" />
+           <div className="mt-8 font-menu text-sm md:text-xl ">
             <h1>Oops!!!No Product Available</h1>
             <h2>Come Back Later.....</h2>
             </div>
