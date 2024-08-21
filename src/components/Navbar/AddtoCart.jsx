feae206192344547e711945d4762d7f91abd11e7
redwanrahat22 <redwan.rahat69@gmail.com>
2024-08-21 12:32:29 +0600

    made the cart responsive

--- src/components/Navbar/AddtoCart.jsx
+++ src/components/Navbar/AddtoCart.jsx
@@ -92,32 +92,38 @@ const AddtoCart = () => {
     return (
         <div className="h-auto font-menu">
 
-                <div className="mt-20">
-                    <div className={`flex items-center space-x-2 justify-center text-4xl  ${screenmode ? 'text-dmgreen' : 'text-lmblue'}`}>
+                <div className="md:mt-20 -mt-8">
+                    <div className={`flex items-center space-x-2 justify-center text-xl md:text-4xl  ${screenmode ? 'text-dmgreen' : 'text-lmblue'}`}>
                     <h1 className="font-semibold font-page">Your Cart</h1>
                     <FaCartArrowDown />
                     </div>
                     
-                    <div className={`flex mt-12 w-2/3 justify-between m-auto shadow-lg bg-white ${screenmode ? "shadow-dmgreen text-black" : "shadow-lmblue "}`}>
-                    <div className="flex  w-11/12 m-auto mt-8 py-4 mb-20">
-                        <div className="w-2/3  space-y-12">
-                        <h1 className="text-xl">Total Products</h1>
-                        <div className="space-y-7">
+                 
+                    <div className={`flex rounded-md mb-16 md:mb-20 mt-8 md:mt-12 w-10/12 md:w-2/3 justify-between m-auto shadow-lg bg-white ${screenmode ? "shadow-dmgreen text-black" : "shadow-lmblue "}`}>
+
+                    <div className="md:flex  md:w-11/12 space-y-8 md:space-y-0 m-auto mt-4 md:mt-8 py-4 md:mb-20">
+                       
+                    <div className=" md:w-2/3 w-10/12 m-auto space-y-4 md:space-y-12">
+                        <h1 className="md:text-xl">Total Products</h1>
+                        
+                        
+                      <div className="md:space-y-7 space-y-8 ">
                         {
                          cartData && cartData.map(data=> <ViewCart data={data} setIsOpen={setIsOpen} DeleteFromCart = {DeleteFromCart}></ViewCart>)
                         }
                         </div>
                         </div>
-                        <div className={`w-1/4 mx-auto ${screenmode ? '' : ''}`}>
+
+                        <div className={`md:w-1/4 pt-8 md:pt-0 w-3/4  mx-auto ${screenmode ? '' : ''}`}>
                           
-                            <h1 className={`text-center py-4 ${screenmode ? 'bg-dmgreen' : 'bg-lmblue'} text-white text-xl`}>Checkout</h1>
+                            <h1 className={`text-center  py-2 md:py-4 ${screenmode ? 'bg-dmgreen' : 'bg-lmblue'} text-white text-lg md:text-xl`}>Checkout</h1>
                             <ul className="list-disc list-inside mt-6 space-y-4 font-page">
                                 {
                                     cartData.map(data=> 
                                       <>
-                                    <div className="flex justify-between w-11/12 m-auto">
-                                    <div className="flex items-center  space-x-2 ">
-                                    <li key={data.prodID} className=" items-center mr">{data.prodName} </li>
+                                    <div className="flex justify-between text-sm md:text-[16px] w-11/12 m-auto">
+                                    <div className="flex items-center space-x-1 md:space-x-2 ">
+                                    <li key={data.prodID} className=" items-center ">{data.prodName} </li>
                                     <div className="flex items-center font-bold">
                                     <h1 className="text-sm text-black "><RxCross2 /></h1>
                                     <h2>1</h2>
@@ -139,23 +145,25 @@ const AddtoCart = () => {
                                 </div>
                                 
                             </ul>
-                                  <button className={`w-1/3 text-white py-1 rounded-full hover:scale-125 ease-in-out duration-300 m-auto block mt-8 px-2 ${screenmode? 'bg-dmgreen' : 'bg-lmblue'}`}>Pay Now</button>
+                                  <button className={`md:w-1/3 text-white py-1 rounded-md md:rounded-full hover:scale-125 ease-in-out duration-300 m-auto block mt-8 px-4 md:px-2 ${screenmode? 'bg-dmgreen' : 'bg-lmblue'}`}>Pay Now</button>
                         </div>
+
+
                         </div>
                     </div>
                 </div>
 
          {isOpen && (
-      <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+      <div className="fixed inset-0 flex items-center justify-center text-black bg-black bg-opacity-50">
         <div
-          className="bg-white rounded-lg shadow-lg p-6 w-full max-w-sm transition-transform  duration-100 ease-in-out"
+          className="bg-white rounded-lg shadow-lg p-4 md:p-6 w-64 md:w-full max-w-sm transition-transform  duration-100 ease-in-out"
           style={style}
         >
-          <h2 className="text-lg font-bold mb-4">Confirm Delete</h2>
-          <p className="mb-6">Are you sure you want to delete this item?</p>
+          <h2 className="md:text-lg font-semibold md:font-bold mb-2 md:mb-4">Confirm Delete</h2>
+          <p className="md:text-[16px] text-sm mb-6">Are you sure you want to delete this item?</p>
           <div className="flex justify-end space-x-4">
-            <button onClick={handleCancel} className="bg-gray-300 text-gray-700 px-4 py-2 rounded">Cancel</button>
-            <button onClick={handleDelete} className="bg-red-500 text-white px-4 py-2 rounded">Delete</button>
+            <button onClick={handleCancel} className="bg-gray-300 text-gray-700 px-4 py-1 md:py-2 text-sm md:text-[16px]  rounded">Cancel</button>
+            <button onClick={handleDelete} className="bg-red-500 text-white px-4 py-1 md:py-2 text-sm md:text-[16px]  rounded">Delete</button>
           </div>
         </div>
       </div>
--- src/components/Navbar/ViewCart.jsx
+++ src/components/Navbar/ViewCart.jsx
@@ -9,22 +9,22 @@ const ViewCart = ({ data,setIsOpen,DeleteFromCart }) => {
 
   return (
     <div>
-      <div className="flex border-y-2 shadow-sm space-x-6">
+      <div className="rounded-lg md:flex border-y-2 shadow-sm space-x-2  md:space-x-6">
         <NavLink to={`/details/${prodBrand}/${prodID}`}>
-        <img src={prodUrl} className={`w-56`} alt="" />
+        <img src={prodUrl} className={`rounded-tl-lg rounded-tr-lg md:w-56 h-full`} alt="" />
         </NavLink>
-        <div className="flex mt-2 font-page justify-between w-full">
+        <div className="md:flex mt-2 font-page justify-between  md:w-full">
         <NavLink to={`/details/${prodBrand}/${prodID}`}>
-          <div className="space-y-4 mb-2  ">
-            <h1 className="font-bold">{prodName}</h1>
-            <h2>{prodBrand}</h2>
-            <h3 className="text-orange-600">${prodPrice}</h3>
+          <div className="md:space-y-4 ml-3 md:ml-0 md:mb-2  ">
+            <h1 className="font-bold text-sm md:text-[16px] ">{prodName}</h1>
+            <h2 className="text-sm md:text-[16px]">{prodBrand}</h2>
+            <h3 className="text-orange-600 text-sm md:text-[16px]">${prodPrice}</h3>
           </div>
           </NavLink>
-          <div className="h-full space-y-11 mr-2">
-            <h1>Quantity: 1</h1>
-            <div className="m-auto w-1/2" onClick={() => setIsOpen(true)}>
-              <h2 onClick={()=>(setIsOpen(true), DeleteFromCart(prodID))} className="text-xl text-red-500 outline outline-1 outline-slate-300 hover:bg-red-100 hover:cursor-pointer duration-200 rounded-sm  p-1 ml-[6px]">
+          <div className="h-full ml-3 md:ml-0    md:space-y-11 space-y-2 md:mr-2">
+            <h1 className="text-sm md:text-[16px]">Quantity: 1</h1>
+            <div className="md:m-auto w-7 justify-end flex md:w-1/2" onClick={() => setIsOpen(true)}>
+              <h2 onClick={()=>(setIsOpen(true), DeleteFromCart(prodID))} className="text-xl mb-3 md:mb-0 text-red-500 outline outline-1 outline-slate-300 hover:bg-red-100 hover:cursor-pointer duration-200 rounded-sm  p-1 md:ml-[6px]">
                 <BsTrash3Fill />
               </h2>
             
